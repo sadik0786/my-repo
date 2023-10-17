9b13c902fe986d3a6303c673818305ba939da70f
Sadik Ali <sadik.ali@5nance.com>
2023-10-17 16:30:37 +0530

    Updates

---
+++ precache-manifest.3e6cabae01e0693b6ebca587e7d9a57a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3ab406487558a0f364fb32632f6bec",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "ca1711b672862f36fddf",
+    "url": "/my-portfolio/static/css/main.fa6c99dd.chunk.css"
+  },
+  {
+    "revision": "d570dc1754db626e1642",
+    "url": "/my-portfolio/static/js/2.2bbd374b.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/my-portfolio/static/js/2.2bbd374b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1711b672862f36fddf",
+    "url": "/my-portfolio/static/js/main.a4698a55.chunk.js"
+  },
+  {
+    "revision": "c2f68ecdf5eeef2896bf",
+    "url": "/my-portfolio/static/js/runtime-main.59db3632.js"
+  },
+  {
+    "revision": "8f4e10fa9ed667c1b88c4a9369d1b801",
+    "url": "/my-portfolio/static/media/Jarvis.8f4e10fa.png"
+  },
+  {
+    "revision": "43fd47ffcd31540886a59b5c364ee491",
+    "url": "/my-portfolio/static/media/bol.43fd47ff.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/my-portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "084f22ffaddd99c212b17d99ba3aa498",
+    "url": "/my-portfolio/static/media/sadik.084f22ff.jpg"
+  }
+]);
